7f7a18e9ddf14b704ac26c31a18dd0023bef0879
unknown <benhamidaessia@gmail.com>
2020-10-08 18:13:23 +0100

    problem solved

--- src/Components/Frequently Used/manyArticles.js
+++ src/Components/Frequently Used/manyArticles.js
@@ -1,36 +1,78 @@
-import React, { Component } from "react";
+import React from "react";
 import "../Activities/Activities.css";
 import Button from "@material-ui/core/Button";
-import Article from "./Article";
-import Dialogue from "./Dialogue";
-import PropTypes from "prop-types";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 
-export default class ManyArticles extends Component {
-  state = { desc: "", openModal: false, titlee: "" };
-  handleOpenModal = ({ desc, titlee }) => {
-    this.setState({ desc, openModal: true, titlee });
+export default function ManyArticles(props) {
+  const { events } = props;
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [t, ti] = React.useState("");
+  const [d, de] = React.useState("");
+  const [p, pa] = React.useState("");
+  const [i, im] = React.useState("");
+
+  function handleClickOpen(e) {
+    ti(e.titlee);
+    de(e.desc);
+    pa(e.paragraph2);
+    im(e.img);
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
   };
-
-  handleCloseModal = () => {
-    this.setState({ openModal: false });
-  };
-
-  render = () => (
+  return (
     <section className="breweries" id="breweries">
       <ul>
-        {this.events.map((e) => (
-          <Article
-            key={e.titlee}
-            {...e}
-            handleOpenModal={this.handleOpenModal}
-          />
+        {events.map((e) => (
+          <li key={e.titlee}>
+            <figure>
+              <img src={e.img} alt={e.titlee}></img>
+              <figcaption>
+                <h3>{e.titlee}</h3>
+              </figcaption>
+            </figure>
+            <p>{e.desc}</p>
+
+            <Button
+              variant="outlined"
+              color="primary"
+              className="browsers"
+              onClick={() => handleClickOpen(e)}
+            >
+              Show more
+            </Button>
+          </li>
         ))}
       </ul>
-      <Dialogue {...this.state} onCloseModal={this.handleCloseModal} />
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">{t}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {d}
+            {p}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </section>
   );
 }
-
-Article.propTypes = {
-  events: PropTypes.array.isRequired,
-};
